6114ded04afff264654aee47517b4ff5b2deb62b
Tailor <ltyiz07@gmail.com>
2026-03-28 17:16:02 +0900

    Fix sign method
    - Using Lower-S as signature s value
    - determineK method returns infinite generator

--- src/ECDSA/Signature.hs
+++ src/ECDSA/Signature.hs
@@ -32,6 +32,7 @@ verify :: Point FG -> Signature -> Integer -> Bool
 verify publicPoint (Signature r s) z
     | r <= 0 || r >= secp256k1Order = False
     | s <= 0 || s >= secp256k1Order = False
+    | s > (secp256k1Order `quot` 2) = False
     | otherwise = 
         let invS = invMod s secp256k1Order
             u = (z * invS) `mod` secp256k1Order
@@ -40,46 +41,49 @@ verify publicPoint (Signature r s) z
             vP = multiplyPoint secp256k1 v publicPoint
             addedPoint = addPoint secp256k1 uG vP
         in case addedPoint of
-            Infinity -> False
+            Infinity   -> False
             Point rx _ -> getValue rx == r
 
+-- | RFC 6979와 BIP146(Low-S)을 준수하는 서명 함수
 sign :: Integer -> Integer -> Signature
-sign e z = Signature (modN r) (modN s)
+sign e z = captureValidSig (determineK e z)
   where
-    modN = flip mod secp256k1Order
-    k = determineK e z
-    r = getValue $ x (multiplyPoint secp256k1 k gPoint)
-    s = (z + r * e) * (invMod k secp256k1Order)
+    n     = secp256k1Order
+    halfN = n `quot` 2
+    captureValidSig []     = error "sign: no valid k found - should be impossible!"
+    captureValidSig (k:ks) =
+        let rPoint = multiplyPoint secp256k1 k gPoint
+        in case rPoint of
+            Infinity   -> captureValidSig ks
+            Point rx _ ->
+                let rVal = getValue rx `mod` n
+                    sRaw = (z + rVal * e) * invMod k n `mod` n
+                    sVal = if sRaw > halfN then n - sRaw else sRaw
+                in if rVal == 0 || sVal == 0
+                    then captureValidSig ks
+                    else Signature rVal sVal
 
-derivePublicPoint :: Integer -> Point FG
-derivePublicPoint e = multiplyPoint secp256k1 e gPoint
-
-determineK :: Integer -> Integer -> Integer
+-- | RFC 6979: 결정론적 k 후보들을 무한히 생성하는 리스트 (Lazy List)
+determineK :: Integer -> Integer -> [Integer]
 determineK e z = go k2 v2
   where
     z' = z `mod` secp256k1Order
-    bz  = intToBytes32 z'
+    bz = intToBytes32 z'
     be = intToBytes32 e
-
-    -- Step 1: 초기화
     v0 = B.replicate 32 0x01
     k0 = B.replicate 32 0x00
-
-    -- Step 2~5: K, V 초기 설정
     k1 = hmac256 k0 (v0 <> B.singleton 0x00 <> be <> bz)
     v1 = hmac256 k1 v0
     k2 = hmac256 k1 (v1 <> B.singleton 0x01 <> be <> bz)
     v2 = hmac256 k2 v1
-
-    -- Step 6: 후보 k 생성 및 검증
     go k v =
         let v'        = hmac256 k v
             candidate = bytesToInteger v'
+            kNext     = hmac256 k     (v' <> B.singleton 0x00)
+            vNext     = hmac256 kNext v'
         in if candidate >= 1 && candidate < secp256k1Order
-           then candidate
-           else
-               -- 범위 밖이면 K, V 갱신 후 재시도
-               let k' = hmac256 k (v' <> B.singleton 0x00)
-                   v''= hmac256 k' v'
-               in go k' v''
+           then candidate : go kNext vNext
+           else go kNext vNext
 
+derivePublicPoint :: Integer -> Point FG
+derivePublicPoint e = multiplyPoint secp256k1 e gPoint
--- test/ECDSA/SignatureSpec.hs
+++ test/ECDSA/SignatureSpec.hs
@@ -8,19 +8,17 @@ import ECDSA.Signature
 import ECDSA.Curve.Secp256k1
 import ECDSA.Utils.Arithmetic
 import ECDSA.Field.FiniteField
--- import qualified Data.Text as T
--- import Utils.Hash
 
 spec :: H.Spec
 spec = do
-    H.describe "DetermineK method test" $ do
+    H.describe "DetermineK method" $ do
         H.it "deterministic k from private-key and hash" $ do
             let e = 0x2bb80d537b1da3e38bd30361aa855686bde0eacd7162fef6a25fe97bf527a25b
                 z = 0xe96cf59c385c8903c6c795d3ce27a49730df2ce0133c830271abac6b5352beb6
-                result = determineK e z
+                (result:_) = determineK e z
                 expected = 0x2580e99c46234781424f08b8f6e9b3d4616c27fd3e3cc559bdb52b85dc2bacb5
             result `H.shouldBe` expected
-    H.describe "ECDSA verify method test" $ do
+    H.describe "ECDSA verify method" $ do
         let px :: FG = 0x887387e452b8eacc4acfde10d9aaf7f6d9a0f975aabb10d006e4da568744d06c
             py :: FG = 0x61de6d95231cd89026e286df3b6ae4a894a3378e393e93a0f45b666329a0ae34
         H.it "test case 1" $ do
@@ -32,17 +30,19 @@ spec = do
             let z = 0x7c076ff316692a3d7eb3c3bb0f8b1488cf72e1afcd929e29307032997a838a3d
                 r = 0xeff69ef2b1bd93a66ed5219add4fb51e11a840f404876325a1e8ffe0529a2c
                 s = 0xc7207fee197d27c618aea621406f6bf5ef6fca38681d82b2f06fddbdce6feab6
-            verify (Point px py) (Signature r s) z `H.shouldBe` True
+                s' = if s > (secp256k1Order `quot` 2) then secp256k1Order - s else s
+            verify (Point px py) (Signature r s') z `H.shouldBe` True
 
-    H.describe "ECDSA sign method test" $ do
+    H.describe "ECDSA sign method" $ do
         let e           = 0x012345
             publicPoint = derivePublicPoint e
             z           = 0x3d67c04d1cc090e5370ce848ee4907abde1381964cd6fbd2e601a00a0c98656a
         H.it "test case 1" $ do
-            let k = determineK e z
+            let (k:_) = determineK e z
                 r = getValue $ x (multiplyPoint secp256k1 k gPoint) -- mod n 추가
                 s = ((z + r * e) * (invMod k secp256k1Order)) `mod` secp256k1Order -- Low-S 사용하도록 수정
-            verify publicPoint (Signature r s) z `H.shouldBe` True
+                s' = if s > (secp256k1Order `quot` 2) then secp256k1Order - s else s
+            verify publicPoint (Signature r s') z `H.shouldBe` True
             -- https://datatracker.ietf.org/doc/html/rfc6979#section-3.2
         H.it "test sign method" $ do
             let sig = sign e z
