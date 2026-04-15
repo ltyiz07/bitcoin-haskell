e2e16691da7259fa72cb5e85be95dbb3647170da
Tailor <ltyiz07@gmail.com>
2026-04-15 20:58:35 +0900

    Add flake.nix support

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "A flake for impl-btc Haskell project";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        haskellPackages = pkgs.haskell.packages.ghc912;
+        project = haskellPackages.developPackage {
+          root = ./.;
+          name = "impl-btc";
+          modifier =
+            drv:
+            pkgs.haskell.lib.addBuildTools drv [
+              pkgs.pkg-config
+              pkgs.zlib
+              pkgs.openssl
+            ];
+        };
+      in
+      {
+        devShells.default = project.env.overrideAttrs (oldAttrs: {
+          buildInputs = oldAttrs.buildInputs ++ [
+            haskellPackages.cabal-install
+            haskellPackages.haskell-language-server
+          ];
+        });
+      }
+    );
+}
